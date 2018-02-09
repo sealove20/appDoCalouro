0b7dcbbe697ee86d8288deab3bad6cfab7efe476
Matheus <matheus.181137@gmail.com>
2018-02-09 18:10:09 -0300

    controle

--- projeto/index.js
+++ projeto/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
+import { StackNavigator } from 'react-navigation';
 
 const App = () => (
    <View>
