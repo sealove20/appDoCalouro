07877cd2c7dbd0cb0e49f6c2a4ecb7384c5920ce
Matheus <matheus.181137@gmail.com>
2018-02-16 22:00:56 -0300

    go horse v2

--- projeto/index.js
+++ projeto/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
-import { StackNavigator } from 'react-navigation';
+import Biblioteca from './src/components/biblioteca';
+import Ru from './src/components/ru';
+import Coordenacao from './src/components/coordenacao';
+import Grade from './src/components/grade';
+import base from './src/components/base.js';
 
-const App = () => (
-   <View>
-      <Header />
-   </View>
-);
-
-AppRegistry.registerComponent('projeto', () => App);
+AppRegistry.registerComponent('projeto', () => base);
--- projeto/src/components/Header.js
+++ projeto/src/components/Header.js
@@ -1,7 +1,33 @@
-import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Image, TouchableOpacity, BackHandler } from 'react-native';
 
-const Header = () => {
+export default class Header extends Component {
+   static navigationOptions = {
+      // header: {
+      //    visible: false,
+      // }
+      title: 'Header',
+   }
+   constructor(props) {
+      super(props);
+      this.handleBackButtonClick = this.handleBackButtonClick.bind(this);
+   }
+
+   componentWillMount() {
+      BackHandler.addEventListener('hardwareBackPress', this.handleBackButtonClick);
+   }
+
+   componentWillUnmount() {
+      BackHandler.removeEventListener('hardwareBackPress', this.handleBackButtonClick);
+   }
+
+   handleBackButtonClick() {
+      this.props.navigation.goBack('false');
+      return true;
+   }
+
+
+render() {
    const {
        container,
        textStyle,
@@ -13,43 +39,97 @@ const Header = () => {
     } = styles;
 
    return (
-   <View>
-      <View style={container}>
-         <Text style={textStyle}>App do Calouro</Text>
-      </View>
+      <View>
+         <View style={container}>
+            <Text style={textStyle}>App do Calouro</Text>
+         </View>
 
-      <View style={imgContainer}>
-      <Image
-         style={{ width: 200, height: 200 }}
-         source={{ uri: 'https://images.emojiterra.com/mozilla/512px/1f393.png' }}
-      />
-      </View>
+         <View style={imgContainer}>
+            <Image
+               style={{ width: 135, height: 140 }}
+               source={require('../../imgs/chapeu.png')}
+            />
+         </View>
          <View style={contentContainer}>
             <View>
-               <TouchableOpacity style={buttonStyle} />
+            <TouchableOpacity
+               style={buttonStyle}
+               onPress={() => this.props.navigation.navigate('Coordenacao')}
+            >
+               <Image
+               style={{
+                  marginLeft: 12,
+                  marginTop: 10,
+                  width: 75,
+                  height: 75
+               }}
+                  source={require('../../imgs/coord.png')}
+               />
+            </TouchableOpacity>
                <Text style={buttonTextStyle2}> Coordenação </Text>
             </View>
 
             <View>
-               <TouchableOpacity style={buttonStyle} />
+            <TouchableOpacity
+               style={buttonStyle}
+               onPress={() => this.props.navigation.navigate('Biblioteca')}
+            >
+               <Image
+               style={{
+                  marginLeft: 14,
+                  marginTop: 10,
+                  width: 75,
+                  height: 75
+               }}
+                  source={require('../../imgs/biblioteca.png')}
+               />
+            </TouchableOpacity>
                <Text style={buttonTextStyle2}> Biblioteca </Text>
             </View>
          </View>
 
          <View style={contentContainer}>
-         <View>
-            <TouchableOpacity style={buttonStyle} />
-            <Text style={buttonTextStyle2}> R.U </Text>
-         </View>
+            <View>
+            <TouchableOpacity
+               style={buttonStyle}
+               onPress={() => this.props.navigation.navigate('Ru')}
+            >
+               <Image
+                  style={{
+                     marginLeft: 14,
+                     marginTop: 15,
+                     width: 75,
+                     height: 75
+                  }}
+                  source={require('../../imgs/ru.png')}
+               />
+            </TouchableOpacity>
+               <Text style={buttonTextStyle2}> R.U </Text>
+            </View>
 
-         <View>
-            <TouchableOpacity style={buttonStyle} />
-            <Text style={buttonTextStyle2}> Grade Curricular</Text>
-         </View>
+
+            <View>
+               <TouchableOpacity
+                  style={buttonStyle}
+                  onPress={() => this.props.navigation.navigate('Grade')}
+               >
+               <Image
+                  style={{
+                     marginLeft: 19,
+                     marginTop: 17,
+                     width: 60,
+                     height: 60
+                  }}
+                  source={require('../../imgs/grade.png')}
+               />
+               </TouchableOpacity>
+                  <Text style={buttonTextStyle2}> Grade Curricular</Text>
+            </View>
          </View>
       </View>
-   );
-};
+      );
+   }
+}
 
 const styles = {
    container: {
@@ -60,8 +140,9 @@ const styles = {
       position: 'relative'
    },
    textStyle: {
-      fontSize: 55,
-      alignSelf: 'center'
+      fontSize: 50,
+      alignSelf: 'center',
+      fontFamily: 'Lobster'
    },
    imgContainer: {
       alignItems: 'center'
@@ -93,7 +174,6 @@ const styles = {
     buttonTextStyle2: {
       alignSelf: 'center',
       justifyContent: 'center',
+      fontFamily: 'Roboto'
    }
 };
-
-export default Header;
